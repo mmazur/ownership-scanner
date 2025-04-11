8687268f5c624798b05ff78b1a4bc39213dacd88
Mariusz Mazur <mmazur@redhat.com>
2025-04-11 15:48:59 +0200

    Run forest

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+wget https://raw.githubusercontent.com/mmazur/ownership-scanner/refs/heads/main/requirements.txt
+wget https://raw.githubusercontent.com/mmazur/ownership-scanner/refs/heads/main/src/ownership_scanner.py
+
+pip install -r requirements.txt
+
+python ./ownership_scanner.py
 
