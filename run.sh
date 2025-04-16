3c8f34c0b9c6a78f6c0d65ef786f570a94919304
Mariusz Mazur <mmazur@redhat.com>
2025-04-16 14:28:55 +0200

    testetesttest

--- run.sh
+++ run.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/mmazur/ownership-scanner/refs/heads/main/requirements.txt
-wget https://raw.githubusercontent.com/mmazur/ownership-scanner/refs/heads/main/src/ownership_scanner.py
+#wget https://raw.githubusercontent.com/mmazur/ownership-scanner/refs/heads/main/requirements.txt
+#wget https://raw.githubusercontent.com/mmazur/ownership-scanner/refs/heads/main/src/ownership_scanner.py
 
-pip install -r requirements.txt
+#pip install -r requirements.txt
 
-python ./ownership_scanner.py
+#python ./ownership_scanner.py
+
+curl -L https://aka.ms/InstallAzureCli | bash
+
+az aks install-cli
+
+az login --identity --object-id  d2becca6-a9cd-41cf-b133-e0c9049b6523
+
+az aks get-credentials --resource-group hcp-underlay-dev-mgmt-1 --name dev-mgmt-1
+
+kubectl get nodes
+
 
