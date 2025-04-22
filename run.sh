10316a61d3d16766ea82c164029133c8ab0bf057
Mariusz Mazur <mmazur@redhat.com>
2025-04-22 14:11:13 +0200

    asdf

--- run.sh
+++ run.sh
@@ -12,13 +12,7 @@
 echo "Install CLI"
 az aks install-cli
 
-echo "Login attempt 1"
-az login --identity --object-id  d2becca6-a9cd-41cf-b133-e0c9049b6523
-
-echo "Sleep"
-sleep 30
-
-echo "Login attempt 2"
+echo "Login attempt"
 az login --identity --client-id  a0174d3f-5600-4951-b8b8-1f9606fc4546
 
 echo "Get Credentials"
